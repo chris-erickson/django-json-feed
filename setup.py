69869165d4f8666d3a7602f2d634f8fef4627c0c
Chris Erickson <chris-erickson@users.noreply.github.com>
2017-06-03 19:42:43 -0500

    Add testing infrastructure for pytest and CircleCI

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 import os
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-    README = readme.read()
-
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-polls',
-    version='0.1',
+    name='django-json-feed',
+    version='1.0',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',
